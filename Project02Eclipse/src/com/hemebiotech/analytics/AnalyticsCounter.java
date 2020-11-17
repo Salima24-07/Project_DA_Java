00c61839bb77c91007790ccabd3e70175d1f4931
salima <salima.hafirassou@hotmail.com>
2020-11-17 21:13:04 +0100

    La méthode main, l'algorithme de map avec itérateur fonctionne lire et écrire

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,43 +1,12 @@
 package com.hemebiotech.analytics;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.FileWriter;
-
 public class AnalyticsCounter {
-	private static int headacheCount = 0;	// initialize to 0
-	private static int rashCount = 0;		// initialize to 0
-	private static int pupilCount = 0;		// initialize to 0
 	
 	public static void main(String args[]) throws Exception {
-		// first get input
-		BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
-		String line = reader.readLine();
-
-		int i = 0;	// set i to 0
-		int headCount = 0;	// counts headaches
-		while (line != null) {
-			i++;	// increment i
-			System.out.println("symptom from file: " + line);
-			if (line.equals("headache")) {
-				headCount++;
-				System.out.println("number of headaches: " + headCount);
-			}
-			else if (line.equals("rush")) {
-				rashCount++;
-			}
-			else if (line.contains("pupils")) {
-				pupilCount++;
-			}
-
-			line = reader.readLine();	// get another symptom
-		}
-		
-		// next generate output
-		FileWriter writer = new FileWriter ("result.out");
-		writer.write("headache: " + headacheCount + "\n");
-		writer.write("rash: " + rashCount + "\n");
-		writer.write("dialated pupils: " + pupilCount + "\n");
-		writer.close();
+		String path_FileSymptoms = "symptoms.txt"; // Chemin relatif de fichier qui contient les symptoms 
+		String path_FileResults = "results.out"; // Fichier résultat qui contient le nombre d'occurence de chaque symptom
+		ReadSymptomDataFromFile read_Symptoms = new ReadSymptomDataFromFile();
+		read_Symptoms.writeAllMapInFile(read_Symptoms.addElementToMap(path_FileSymptoms), path_FileResults); 
 	}
+	
 }
