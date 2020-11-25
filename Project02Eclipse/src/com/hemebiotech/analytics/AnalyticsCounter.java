d6c34ff4d4d5f266de0fb609f622cbab3da00b61
salima <salima.hafirassou@hotmail.com>
2020-11-25 17:15:35 +0100

    mise a jour de la class ReadSymptomDataFromFile: ajout de la methode sortMap, mise a jour de la methode main, documentation du code.

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,12 +1,23 @@
 package com.hemebiotech.analytics;
 
+import java.util.Map;
+
 public class AnalyticsCounter {
 	
+	/**
+	 * Initialize the paths of the source and result files, 
+	 * and send them by parameter by calling the methods "addElementToMap" and "writeAllMapInFile"
+	   of the class "ReadSymptomDataFromFile" 
+	 * @author salima
+	 * @param args
+	 */
+	
 	public static void main(String args[]) throws Exception {
-		
-		String path_FileSymptoms = "symptoms.txt"; // chemin relatif de fichier qui contient les symptoms 
-		String name_FileResults = "results.out"; // fichier resultat qui contient le nombre d'occurence de chaque symptom
-		ReadSymptomDataFromFile read_Symptoms = new ReadSymptomDataFromFile();
-		read_Symptoms.writeAllListInFile(read_Symptoms.GetSymptoms(path_FileSymptoms), name_FileResults);
+
+		String pathFileSymptoms = "symptoms.txt"; // chemin relatif de fichier qui contient les symptoms 
+		String nameFileResults = "results.out"; // fichier resultat qui contient le nombre d'occurence de chaque symptom
+		ReadSymptomDataFromFile readSymptom = new ReadSymptomDataFromFile();
+		Map<String, Integer> symptoms = readSymptom.addElementToMap(pathFileSymptoms);
+		readSymptom.writeAllMapInFile(symptoms, nameFileResults); 
 	}
 }
